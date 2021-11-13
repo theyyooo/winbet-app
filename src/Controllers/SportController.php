123c76594508199e4b7a3db3fcf28e68cc974fbe
theyyooo <polette.theo@hotmail.fr>
2021-11-13 11:20:58 +0100

    prepare first Api request

--- src/Controllers/SportController.php
+++ src/Controllers/SportController.php
@@ -2,6 +2,7 @@
 
 require_once '../src/Renderer.php';
 require_once '../src/Models/DAO/DAOSport.php';
+require_once '../src/Models/DAO/DAOApiFootball.php';
 
 class SportController
 {
@@ -10,7 +11,12 @@ class SportController
     {
         $DAOSport = new DAOSport(Singleton::getInstance()->cnx);
         $sports = $DAOSport->findAll();
-        $data = compact('sports');
+
+        $api = new ApiFootball("ad752d786e584657b7fdb9d7390e978d");
+        $results = $api->getAllNextMatch();
+
+        $data = compact('sports', 'results');
+        var_dump($data);
         echo Renderer::render('sports.php', $data);
     }
 
